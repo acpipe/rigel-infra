39dc160bb64332503e82b2bd3d9a48fa260654e6
huming <1023598954@qq.com>
2019-11-12 14:16:42 +0800

    add BufferTrigger

---
+++ src/main/java/com/rigel/buffer/BufferTrigger.java
@@ -0,0 +1,13 @@
+package com.rigel.buffer;
+
+/**
+ * @author huming on 2019-11-12.
+ * 内存聚合消费.
+ * 应用:
+ * DE内存更新
+ * 数据上报统计(广告打点、曝光)
+ * binlog batch更新es索引.
+ * ...
+ */
+public class BufferTrigger {
+}
